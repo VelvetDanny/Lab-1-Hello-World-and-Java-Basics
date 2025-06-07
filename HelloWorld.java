87399c171878adfa85f927aa5f99119fc7313916
VelvetDanny <dannylarose10@Gmail.com>
2025-06-07 14:25:11 -0500

    Revising...

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,21 +1,23 @@
-
 public class HelloWorld  { // main class 
-  public static void main(String[] args) { //main function
-    System.out.println( // main print function
-        "Hello, World!\n" // prints Hello World
-        + "Daniel De La Rosa\n" // Prints my name
-        + "Scott Weiland\n" // Prints my favorite Celebrity
-        + "Dishes\n" // Least Favorite Chore
-        + "I would rather be playing Roblox" // Prints what I'd rather be doing
-    );
+  public static void main(String[] args) { //Main Function
+
+    System.out.println("Hello, World!"); // Prints Hello World to the output
+    System.out.println(""); //Creates a line break to separate Hello World output from questions
+    System.out.println("Answers to Questions: ");//Prints "Answers to Questions" to separate from Hello World
+    System.out.println("Daniel De La Rosa"); // Prints my name to the output
+    System.out.println("Scott Weiland"); // Prints my favorite Celebrity to the output
+    System.out.println("Dishes"); // Least Favorite Chore to do, to the output
+    System.out.println("Empanadas"); //Prints favorite food
+    System.out.println("I would rather be playing Roblox"); // Prints what I'd rather be doing
+    
   }
 }
 /* 1. What does the main() method represent in a Java program?
  * The main method in java represents the initial execution of the java program/Where the JVM starts it's execution.
  * 
  * 2. Why is System.out.println() used to display output?
- * The System.out.println() is used to display output because it enumerates upon the system then the output of the system then it calls the 
- * function to give the string a new line for it to use in the output.
+ * The System.out.println() calls upon the output stream to display a message and the println() function is used to
+ * display the message of your choice using a string within the argument.
  * 
  * 3. What is the purpose of comments in the program?
  * The purpose of comments is for developers working on the same page to 
